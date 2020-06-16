8f6113277ed1e63cb7b04cd47c09234c0b82344a
Joshua Schmitt <joshua@pixelcubed.com>
2020-06-16 18:45:39 -0500

    fix add scanner script

--- scripts/add-scanner.sh
+++ scripts/add-scanner.sh
@@ -6,5 +6,11 @@ read -p "Scanner public key: " SCANNER_KEY
 
 echo "Adding scanner $SCANNER_NAME..."
 
+IFS=' '
+
+read -a $SCANNER_KEY_ARRAY <<< "$text"
+
+su -c "gvmd --create-scanner=$SCANNER_NAME --scanner-type=OpenVAS --scanner-host='/sockets/$SCANNER_KEY_ARRAY[2].sock'" gvm
+
 echo "$SCANNER_KEY\n" >> /data/scanner-ssh-keys/authorized_keys
 chown gvm:gvm -R /data/scanner-ssh-keys
