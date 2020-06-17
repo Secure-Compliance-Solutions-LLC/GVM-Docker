09d5688ac1248c2bddbd6398818503061685df85
Joshua Schmitt <joshua@pixelcubed.com>
2020-06-17 12:14:35 -0500

    change add scanner script to use id

--- scripts/add-scanner.sh
+++ scripts/add-scanner.sh
@@ -2,15 +2,12 @@
 set -Eeuo pipefail
 
 read -p "Scanner Name: " SCANNER_NAME
+read -p "Scanner ID: " SCANNER_ID
 read -p "Scanner public key: " SCANNER_KEY
 
 echo "Adding scanner $SCANNER_NAME..."
 
-IFS=' '
-
-read -a $SCANNER_KEY_ARRAY <<< "$text"
-
-su -c "gvmd --create-scanner=$SCANNER_NAME --scanner-type=OpenVAS --scanner-host='/sockets/$SCANNER_KEY_ARRAY[2].sock'" gvm
+su -c "gvmd --create-scanner=$SCANNER_NAME --scanner-type=OpenVAS --scanner-host='/sockets/$SCANNER_ID.sock'" gvm
 
 echo "$SCANNER_KEY\n" >> /data/scanner-ssh-keys/authorized_keys
 chown gvm:gvm -R /data/scanner-ssh-keys
