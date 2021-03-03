132c282640154c0aabf61e2fdb0464548f96f6a9
Austin Songer <austinvernsonger@protonmail.com>
2021-03-02 22:13:41 -0600

    #Ensures Postgres Shutdowns

--- scripts/start.sh
+++ scripts/start.sh
@@ -336,4 +336,6 @@ echo ""
 echo "++++++++++++++++"
 echo "+ Tailing logs +"
 echo "++++++++++++++++"
-tail -F /usr/local/var/log/gvm/*
+tail -F /usr/local/var/log/gvm/* &
+
+wait $! #Ensures Postgres Shutdowns
