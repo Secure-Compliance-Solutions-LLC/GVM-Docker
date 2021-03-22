3dbbc51f93eae14d2563f68605d9dd24c0bacd44
Masaya-A <68965261+Masaya-A@users.noreply.github.com>
2021-03-22 15:47:13 +0900

    Added socket mode to ospd-openvas command
    Added a socket mode option to the ospd-openvas command, then more reliable.
    (And deleted a chmod command which isn't needed anymore)

--- scripts/start.sh
+++ scripts/start.sh
@@ -257,13 +257,11 @@ sed -i "s/^relayhost.*$/relayhost = ${RELAYHOST}:${SMTPPORT}/" /etc/postfix/main
 service postfix start
 
 echo "Starting Open Scanner Protocol daemon for OpenVAS..."
-ospd-openvas --log-file /usr/local/var/log/gvm/ospd-openvas.log --unix-socket /var/run/ospd/ospd.sock --log-level INFO
+ospd-openvas --log-file /usr/local/var/log/gvm/ospd-openvas.log --unix-socket /var/run/ospd/ospd.sock --socket-mode 0o666 --log-level INFO
 
 while  [ ! -S /var/run/ospd/ospd.sock ]; do
 	sleep 1
 done
-
-chmod 666 /var/run/ospd/ospd.sock
 
 echo "Creating OSPd socket link from old location..."
 rm -rf /tmp/ospd.sock
