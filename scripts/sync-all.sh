ed7588e34bfa1a6c85bb88034719599eb79fb640
Joshua Schmitt <joshua@pixelcubed.com>
2021-05-20 22:04:37 -0500

    add tar download for first sync

--- scripts/sync-all.sh
+++ scripts/sync-all.sh
@@ -1,5 +1,27 @@
 #!/usr/bin/env bash
 
+if [ ! -f "/firstsync" ]; then
+	echo "Downloading data TAR to speed up first sync..."
+	curl -o /tmp/data.tar.xz https://vulndata.securecompliance.solutions/file/VulnData/data.tar.xz # This file is updated at 0:00 UTC every day
+	mkdir /tmp/data
+	
+	echo "Extracting data TAR..."
+	tar --extract --file=/tmp/data.tar.xz --directory=/tmp/data
+	
+	mv /tmp/data/nvt-feed/* /usr/local/var/lib/openvas/plugins
+	mv /tmp/data/gvmd-data/* /usr/local/var/lib/gvm/data-objects
+	mv /tmp/data/scap-data/* /usr/local/var/lib/gvm/scap-data
+	mv /tmp/data/cert-data/* /usr/local/var/lib/gvm/cert-data
+	
+	chown gvm:gvm -R /usr/local/var/lib/openvas/plugins
+	chown gvm:gvm -R /usr/local/var/lib/gvm/data-objects
+	chown gvm:gvm -R /usr/local/var/lib/gvm/scap-data
+	chown gvm:gvm -R /usr/local/var/lib/gvm/cert-data
+	
+	rm /tmp/data.tar.xz
+	rm -r /tmp/data
+fi
+
 echo "Updating NVTs..."
 su -c "rsync --compress-level=9 --links --times --omit-dir-times --recursive --partial --quiet rsync://feed.community.greenbone.net:/nvt-feed /usr/local/var/lib/openvas/plugins" gvm
 sleep 5
