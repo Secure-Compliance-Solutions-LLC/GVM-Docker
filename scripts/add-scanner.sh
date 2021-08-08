5e8cc770f4c0db1ead128517ee3e3ec26018c8a1
Josef Fröhle <froehle@b1-systems.de>
2021-08-08 16:13:00 +0200

    remove newline in echo append ssh

--- scripts/add-scanner.sh
+++ scripts/add-scanner.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
+touch /var/lib/gvm/.ssh/authorized_keys
+chmod 600 /var/lib/gvm/.ssh/authorized_keys
 
 read -p "Scanner Name: " SCANNER_NAME
 read -p "Scanner ID: " SCANNER_ID
@@ -9,5 +11,5 @@ echo "Adding scanner $SCANNER_NAME..."
 
 su -c "gvmd --create-scanner='$SCANNER_NAME' --scanner-type=OpenVAS --scanner-host='/sockets/$SCANNER_ID.sock'" gvm
 
-echo "$SCANNER_KEY\n" >>/var/lib/gvm/.ssh/authorized_keys
+echo "${SCANNER_KEY}" >>/var/lib/gvm/.ssh/authorized_keys
 chown gvm:gvm -R /var/lib/gvm/.ssh
