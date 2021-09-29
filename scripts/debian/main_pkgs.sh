3976c1ba6319621053e5ac72087624a37fef60ac
Josef Fröhle <froehle@b1-systems.de>
2021-09-29 09:27:32 +0200

    Update for Debian11 impacket package name

--- scripts/debian/main_pkgs.sh
+++ scripts/debian/main_pkgs.sh
@@ -235,7 +235,7 @@ sudo apt-get install -y --no-install-recommends \
 
 # Install optional dependencies for openvas-scanner
 sudo apt-get install -y \
-    python-impacket \
+    python3-impacket \
     libsnmp-dev
 
 curl -sSL https://github.com/greenbone/openvas-scanner/archive/refs/tags/v${openvas_scanner_version}.tar.gz -o ${SOURCE_DIR}/openvas-scanner-${openvas_scanner_version}.tar.gz
